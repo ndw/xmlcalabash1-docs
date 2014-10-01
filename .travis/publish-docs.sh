5aa7c2432e3621606f9f7447e2d7ed76237df64e
Norman Walsh <ndw@nwalsh.com>
2014-10-01 15:37:52 -0500

    Attempt to get Travis to update the gh-pages

---
+++ .travis/publish-docs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set | grep TRAVIS
+
+if [ "$TRAVIS_REPO_SLUG" == "$GIT_PUB_REPO" ]; then
+    echo -e "Setting up for publication...\n"
+
+    cd $HOME
+    git config --global user.email ${GIT_EMAIL}
+    git config --global user.name ${GIT_NAME}
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GIT_PUB_REPO} gh-pages > /dev/null
+
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        echo -e "Publishing docs...\n"
+
+        ROOT=/home/travis/build/ndw/xmlcalabash1-docs
+
+        cd gh-pages
+        rsync -var --delete $ROOT/build/ref/ ./ref/
+        cp $ROOT/dist/*.zip ./download/
+
+        #git add -f .
+        #git commit -m "Successful travis build $TRAVIS_BUILD_NUMBER"
+        #git push -fq origin gh-pages > /dev/null
+
+        echo -e "Published specification to gh-pages.\n"
+    else
+        echo -e "Publication cannot be performed on pull requests.\n"
+    fi
+fi
